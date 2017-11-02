d5df7a4b74594b5f226850406b8107b1a7800bd1
javadoug <doug@onvelocity.com>
2017-11-02 13:28:52 -0400

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cs-cypher-ql
 fluent api for simple cypher queries 
+
+# Goals
+ 	Write cyphers with syntactic sugar from fluent api:
+ 		• only see valid options returned from each element in context
+ 		• throw errors when bad cypher is written
+ 		• reuse parts of a cypher to reduce code clutter
+
+I want to be able to write something like this
+```
+    MATCH (u:Person {key: $personKey})-[:OWNS]->(ctx:Item {key: $itemKey})
+    OPTIONAL MATCH (ctx)-[v:Color]-(:Color)
+    OPTIONAL MATCH (ctx)<-[s:Size]-()
+    RETURN "CTX" as type, toFloat(count(DISTINCT s)) as count,
+        ctx.name as name,
+        collect(ctx.desc)[0] as latestDesc,
+        collect(distinct toFloat(v.price)) as prices
+```
+
+ Something like:
+```
+    Cypher.QL.Match.Node('u', 'Person').WithParam('key', '$personKey').Via.Relationship('', 'OWNS').To.Node('ctx', 'Item')
+        .Match.Node('ctx').Via.Relationship('v', 'Color').Via.Node('', 'Color')
+        .Match.Node('ctx').Via.Relationship('s', 'Size').Via.Node()
+        .Return.Literal('CTX').As('type')
+        .And.Float.Count.Distinct.Value('s').As('count')
+        .And.Collect.Value('ctx.desc').Sub(0).As('latestDesc')
+        .And.Collect.Distinct.Float.Value('v.price').As('prices')
+        .toString();
+```
---
+++ src/CypherQL.js
@@ -0,0 +1,112 @@
+export const Cypher = {
+	get QL() {
+		const query = [];
+		return {
+			PeriodicCommit(size = 1000) {
+				query.push(` USING PERIODIC COMMIT ${size}`);
+				return this;
+			},
+			get LoadCSV() {
+				query.push(` LOAD CSV`);
+				return this;
+			},
+			get WithHeaders() {
+				query.push(` WITH HEADERS`);
+				return this;
+			},
+			From(url) {
+				query.push(` FROM ${url}`);
+				return this;
+			},
+			get AsLine() {
+				query.push(` AS line`);
+				return this;
+			},
+			get Match() {
+				query.push(` MATCH`);
+				return this;
+			},
+			Node(name, label) {
+				query.push({type: 'NODE', name, label, params: [], toString() {
+					const name = this.name ? this.name : '';
+					const label = this.label ? ':' + this.label : '';
+					const params = this.params.map((p) => `${p.name}: ${p.value}`);
+					if (params.length) {
+						return `(${name}${label} {${params.join(', ')}})`;
+					}
+					return `(${name}${label})`;
+				}});
+				return this;
+			},
+			WithParam(name, value) {
+				const obj = query[query.length - 1];
+				if (!(obj && typeof obj === 'object')) {
+					throw new TypeError('expect Node or Relationship');
+				}
+				obj.params.push({name, value});
+				return this;
+			},
+			UsingIndex(name) {
+				const obj = query.slice().reverse().filter((obj, token) => obj && obj.name)[0];
+				if (!(obj && typeof obj === 'object')) {
+					throw new TypeError(`expect a Node clause before this clause, like "Node('foo', 'bar').UsingIndex('key')"`);
+				}
+				query.push(` USING INDEX ${obj.name}:${obj.label}(${name})`);
+				return this;
+			},
+			get Merge() {
+				query.push(` MERGE`);
+				return this;
+			},
+			get Via() {
+				query.push(`-`);
+				return this;
+			},
+			Relationship(name, label) {
+				query.push({type: 'RELATIONSHIP', name, label, params: [], toString() {
+					const name = this.name ? this.name : '';
+					const label = this.label ? ':' + this.label : '';
+					const params = this.params.map((p) => `${p.name}: ${p.value}`);
+					if (params.length) {
+						return `[${name}${label} {${params.join(', ')}}]`;
+					}
+					return `[${name}${label}]`;
+				}});
+				return this;
+			},
+			get To() {
+				const obj = query[query.length - 1];
+				if (!(obj && typeof obj === 'object')) {
+					throw new TypeError(`expect a Node or Relationship like "Node('foo','Foo').To.Relationship(...)"`)
+				}
+				if (obj.type === 'NODE') {
+					query.push('<-');
+				} else {
+					query.push('->');
+				}
+				return this;
+			},
+			get On() {
+				query.push(` ON`);
+				return this;
+			},
+			get Create() {
+				query.push(` CREATE`);
+				return this;
+			},
+			SetFieldsFromLine(name, fields) {
+				let q;
+				if (query[query.length - 1] === ' MATCH') {
+					q = fields.map((field) => `${field}: coalesce(line.${field}, ${name}.${field})`).join(', ');
+				} else {
+					q = fields.map((field) => `${field}: line.${field}`).join(', ');
+				}
+				query.push(` SET ${name} += {${q}}`);
+				return this;
+			},
+			toString() {
+				return query.join('').trim();
+			}
+		}
+	}
+};
